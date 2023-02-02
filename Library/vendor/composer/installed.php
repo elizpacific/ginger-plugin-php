32ce16d8488d50a2abcbb754003f388dfde95115
elizpacific <elizpacific@gmail.com>
2023-02-02 13:43:24 +0200

    dev

---
+++ Library/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5d9f55206b5b20f8542e0500afa724ed9c95d480',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5d9f55206b5b20f8542e0500afa724ed9c95d480',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'gingerpayments/ginger-php' => array(
+            'pretty_version' => '2.2.0',
+            'version' => '2.2.0.0',
+            'reference' => '38c32c48a4ce0e5344bbf591419803d0135e08d4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gingerpayments/ginger-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'gingerpayments/ginger-plugin-sdk' => array(
+            'pretty_version' => '1.4.4',
+            'version' => '1.4.4.0',
+            'reference' => '9419407e9b879955661e8672cb0cb4cc8e62c063',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gingerpayments/ginger-plugin-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
